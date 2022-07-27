5eddc905cd302c49517d3324d21b638d5c998868
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-07-27 17:39:14 +0200

    old tags done

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -36,7 +36,7 @@
                         <h6>Tags</h6>
                        @foreach ($tags as $tag)
                        <div class="form-check form-check-inline">
-                           <input class="form-check-input" type="checkbox" id="{{$tag->slug}}" value="{{$tag->id}}" name="tags[]">
+                           <input class="form-check-input" type="checkbox" id="{{$tag->slug}}" value="{{$tag->id}}" name="tags[]" {{in_array($tag->id, old('tags', [])) ? 'checked' : ''}}>
                            <label for="form-check-label" for="{{$tag->slug}}">{{$tag->name}}</label>
                        </div>
                        @endforeach 
